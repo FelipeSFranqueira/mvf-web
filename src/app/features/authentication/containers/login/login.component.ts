bf22c66df1441e04f9484d1bb99165504e6fdeef
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2023-09-16 20:39:11 -0300

    feat: store jwt on a cookie after login

--- src/app/features/authentication/containers/login/login.component.ts
+++ src/app/features/authentication/containers/login/login.component.ts
@@ -1,7 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationFacade } from '../../authentication.facade';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { LoginRequest } from '../../models/login-request.model';
 
+@UntilDestroy()
 @Component({
   selector: 'mvf-auth-root',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginLoading$?: Observable<boolean>;
+  loginError$?: Observable<boolean>;
+  loginSuccess$?: Observable<boolean>;
+
+  loginForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
+  });
+
+  constructor(
+    private facade: AuthenticationFacade,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginLoading$ = this.facade.loginLoading$;
+    this.loginSuccess$ = this.facade.loginSuccess$;
+    this.loginError$ = this.facade.loginError$;
+
+    this.loginError$
+      .pipe(untilDestroyed(this))
+      .subscribe((hasError) =>
+        hasError ? this.toastr.error('Email e/ou senha inválidos.') : null
+      );
+    this.loginSuccess$
+      .pipe(untilDestroyed(this))
+      .subscribe((success) =>
+        success ? this.toastr.success('Sucesso') : null
+      );
+  }
+
+  login(): void {
+    const loginData: LoginRequest = {
+      email: this.loginForm.controls.email.value,
+      password: this.loginForm.controls.password.value,
+    };
+
+    this.facade.login(loginData);
+  }
+}
--- src/app/features/authentication/state/authentication.state.ts
+++ src/app/features/authentication/state/authentication.state.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class AuthenticationState {
   private _isRegistering$ = new BehaviorSubject<boolean>(false);
   private _isRegistered$ = new BehaviorSubject<boolean>(false);
   private _error$ = new BehaviorSubject<boolean>(false);
+  private _loginLoading$ = new Subject<boolean>();
+  private _loginSuccess$ = new Subject<boolean>();
+  private _loginError$ = new Subject<boolean>();
 
   get isRegistering$(): Observable<boolean> {
     return this._isRegistering$.asObservable();
@@ -17,6 +20,18 @@ export class AuthenticationState {
 
   get error$(): Observable<boolean> {
     return this._error$.asObservable();
+  }
+
+  get loginLoading$(): Observable<boolean> {
+    return this._loginLoading$.asObservable();
+  }
+
+  get loginSuccess$(): Observable<boolean> {
+    return this._loginSuccess$.asObservable();
+  }
+
+  get loginError$(): Observable<boolean> {
+    return this._loginError$.asObservable();
   }
 
   register(): void {
@@ -31,5 +46,21 @@ export class AuthenticationState {
   dispatchError(): void {
     this._error$.next(true);
     this._isRegistering$.next(false);
+  }
+
+  login(): void {
+    this._loginLoading$.next(true);
+  }
+
+  finishLogin(): void {
+    this._loginLoading$.next(false);
+    this._loginError$.next(false);
+    this._loginSuccess$.next(true);
+  }
+
+  dispatchLoginError(): void {
+    this._loginError$.next(true);
+    this._loginLoading$.next(false);
+    this._loginSuccess$.next(false);
   }
 }
